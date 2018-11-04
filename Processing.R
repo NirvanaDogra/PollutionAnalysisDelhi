8dda4c5c1f5f77a716682c83696903540322b089
NirvanaDogra <32649087+NirvanaDogra@users.noreply.github.com>
2018-11-05 00:07:26 +0530

    data preprocessing

--- Processing.R
+++ Processing.R
@@ -4,4 +4,26 @@ summary(RawData)
 #coverting the character to  integer
 RawData$AT<-as.numeric(RawData$AT)
 
-summary(RawData$AT)
+
+#checing the values of temp all are null
+RawData$Temp
+max(RawData$Temp)
+table(RawData$Temp)
+
+
+RawData$WS #is a character type need conversion
+RawData$WS<-as.numeric(RawData$WS)
+RawData$WS
+summary(RawData$WS)
+
+#comverting the data to numberic
+RawData$VWS<-as.numeric(RawData$VWS)
+RawData$NH3<-as.numeric(RawData$NH3)
+RawData$NO<-as.numeric(RawData$NO)
+RawData$NO2<-as.numeric(RawData$NO2)
+RawData$NOx<-as.numeric(RawData$NOx)
+RawData$Ozone<-as.numeric(RawData$Ozone)
+RawData$SO2<-as.numeric(RawData$SO2)
+RawData$CO<-as.numeric(RawData$CO)
+
+
