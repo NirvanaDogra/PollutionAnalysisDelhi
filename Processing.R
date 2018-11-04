0406159ede72cf797f83b343a7d6a668724b4e73
NirvanaDogra <32649087+NirvanaDogra@users.noreply.github.com>
2018-11-04 23:54:54 +0530

    data preprocessing

--- Processing.R
+++ Processing.R
@@ -1,8 +1,7 @@
 View(RawData)
 summary(RawData)
 
+#coverting the character to  integer
+RawData$AT<-as.numeric(RawData$AT)
 
-#converting the data into integer values ie is a more workable format
-write.csv(RawData, file="Data.csv")
-Data=read.csv("Data.csv")
-summary(Data)
+summary(RawData$AT)
